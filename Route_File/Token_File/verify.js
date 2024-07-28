b8bd5c94949cc5785eb78b70e3785cfd7b5195f1
Bidhan Deb Nath <Bidhandebnath15.09.2001@gmail.com>
2024-07-28 21:33:44 +0600

    Update

--- Route_File/Token_File/verify.js
+++ Route_File/Token_File/verify.js
@@ -6,13 +6,13 @@ token.verify = (TokenID, PhoneNumber, callback) => {
     readUserDataLibrary('Tokens', TokenID, (error, tokenData) => {
         if (!error && tokenData) {
             
-            if (tokenData.Phone_number === PhoneNumber && tokenData.Validation_time < Date.now()) {
+            if (tokenData.Phone_number === PhoneNumber && tokenData.Validation_time > Date.now()) {
                 callback(true);
             } else {
                 console.log('Token validation failed: PhoneNumber mismatch or token expired.');
                 callback(false);
             }
-            
+
         } else {
             console.log('Error reading token data or token not found:', error);
             callback(false);
