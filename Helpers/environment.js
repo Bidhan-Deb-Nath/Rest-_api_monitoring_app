05fe509fa7c8c5ce47aa0f0879f6e3a3732f0d7c
Bidhan Deb Nath <Bidhandebnath15.09.2001@gmail.com>
2024-07-29 13:13:50 +0600

    update

--- Helpers/environment.js
+++ Helpers/environment.js
@@ -1,7 +1,7 @@
 const environment = {};
 
-environment.staging = { portName: 'Staging', portNumber: 2000, secretKey: 'zmcxbvnwtreqyuioaljkdghsf' };
-environment.production = { portName: 'Production', portNumber: 4000, secretKey: 'jkdghsfnwtreqyuioaljzmcxbv' };
+environment.staging = { portName: 'Staging', portNumber: 2000, secretKey: 'zmcxbvnwtreqyuioaljkdghsf', minimumTime : 1, maximumTime : 6 };
+environment.production = { portName: 'Production', portNumber: 4000, secretKey: 'jkdghsfnwtreqyuioaljzmcxbv', minimumTime : 1, maximumTime : 6  };
 
 const currentEnvironment = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : environment.staging;
 
--- Route_File/Check_File/postMethod.js
+++ Route_File/Check_File/postMethod.js
@@ -1,9 +1,22 @@
 const check = require('./check');
+const { minimumTime, maximumTime } = require('../../Helpers/environment');
 
 check.postMethod = (requestedProperties, callback) => {
     const protocol = typeof (requestedProperties.body.protocol) === 'string' && ['http', 'https'].indexOf(requestedProperties.body.protocol) > -1 ? requestedProperties.body.protocol : false;
-    
+
     const url = typeof (requestedProperties.body.url) === 'string' && requestedProperties.body.url.trim().length > 0 ? requestedProperties.body.url : false;
+
+    const method = typeof (requestedProperties.body.method) === 'string' && ['POST', 'GET','PUT', 'DELETE'].indexOf(requestedProperties.body.method) > -1 ? requestedProperties.body.method : false;
+    
+    const successCode = typeof (requestedProperties.body.successCode) === 'object' && requestedProperties.body.successCode instanceof Array ? requestedProperties.body.successCode : false;
+
+    const timeOutSeconds = typeof (requestedProperties.body.timeOutSeconds) === 'number' && requestedProperties.body.timeOutSeconds % 1 === 0 && requestedProperties.body.timeOutSeconds >= minimumTime && requestedProperties.body.timeOutSeconds <= maximumTime ? requestedProperties.body.timeOutSeconds : false;
+
+    if (protocol && url && method && successCode && timeOutSeconds) {
+        const token = typeof (requestedProperties.headersObject.token) === 'string' ? requestedProperties.headersObject.token : false;
+    } else {
+        callback(400, { Error: 'You have a problem in your inputs'});
+    }
 };
 
 
