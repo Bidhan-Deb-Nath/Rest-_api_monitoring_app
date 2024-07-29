9e3418495a7cc8632b8ac5ce232973a8f659d2dd
Bidhan Deb Nath <Bidhandebnath15.09.2001@gmail.com>
2024-07-29 21:59:34 +0600

    Update check create

--- Route_File/Check_File/postMethod.js
+++ Route_File/Check_File/postMethod.js
@@ -1,5 +1,11 @@
 const check = require('./check');
-const { minimumTime, maximumTime } = require('../../Helpers/environment');
+const { minimumTime, maximumTime, maximumChecks} = require('../../Helpers/environment');
+const { readUserDataLibrary } = require('../../Library/readUserDataLibrary');
+const { verify } = require('../Token_File/verify');
+const { createRandomString } = require('../../Helpers/utilities');
+const { createUserDataLibrary } = require('../../Library/createUserDataLibrary');
+const { updateUserDataLibrary } = require('../../Library/updateUserDataLibrary');
+
 
 check.postMethod = (requestedProperties, callback) => {
     const protocol = typeof (requestedProperties.body.protocol) === 'string' && ['http', 'https'].indexOf(requestedProperties.body.protocol) > -1 ? requestedProperties.body.protocol : false;
@@ -14,6 +20,51 @@ check.postMethod = (requestedProperties, callback) => {
 
     if (protocol && url && method && successCode && timeOutSeconds) {
         const token = typeof (requestedProperties.headersObject.token) === 'string' ? requestedProperties.headersObject.token : false;
+        readUserDataLibrary('Tokens', token, (error, tokenData) => {
+            if (!error && tokenData) {
+                const phoneNumber = tokenData.Phone_number;
+                readUserDataLibrary('Users', phoneNumber, (error, userData) => {
+                    if (!error && userData) {
+                        verify(token, phoneNumber, tokenIsValid => {
+                            if (tokenIsValid) {
+                                const userChecks = typeof (userData.checks) === 'object' && userData.checks instanceof Array ? userData.checks : [];
+                                if (userChecks.length < maximumChecks) {
+                                    const checkId = createRandomString(20);
+                                    const checkData = { checkId, phoneNumber, protocol, url, method, successCode, timeOutSeconds };
+                                    createUserDataLibrary('Checks', checkId, checkData, (error) => { 
+                                        if (!error) { 
+                                            userData.checks = userChecks;
+                                            userData.checks.push(checkId);
+                                            updateUserDataLibrary('Users', phoneNumber, userData, (error) => { 
+                                                if (!error) {
+                                                    callback( 200, { Message: `Check created successfully.Check data is : ${checkData}` });
+                                                 } else {
+                                                    callback(500, { Error: 'There was  a problem in server side!' })
+                                                };
+                                            });
+                                            callback(null, { checkId });
+                                        } else {
+                                            callback(500, { Error: 'There was  a problem in server side!' });                                            
+                                        }
+                                     });
+                                } else {
+                                    callback(403, { Error: 'User has already reached max checked limit!' });                   
+                                }
+                            } else {
+                                callback(403, { Error: 'Authentication failure!' });    
+                            }
+                        })
+                    } else {
+                        callback(404, { Error: 'User not found!' });
+                        
+                    }
+                })
+            } else {
+                callback(403, { Error: 'Authentication problem!' });
+                
+            }
+        });
+
     } else {
         callback(400, { Error: 'You have a problem in your inputs'});
     }
